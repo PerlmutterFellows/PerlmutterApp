8cb0eb526811881181579ba1174590373464e51b
dodellmitchell@gmail.com <YOUR@EMAIL.com>
2020-05-24 18:36:10 -0400

    Reseting everything

--- perlmutter_app/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  root 'authentication#login'
-  #get 'authentication#logout'
-  #get 'authentication#signup'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
