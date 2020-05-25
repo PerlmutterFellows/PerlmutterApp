c557b49a94e0661a8471d9a4e820e2d5515ad624
dodellmitchell@gmail.com <dodellmitchell@gmail.com>
2020-05-25 03:12:56 -0400

    Updated link for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "PerlmutterSummer2020_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://perlmutter-production.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://perlmutter-staging.herokuapp.com/' }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
