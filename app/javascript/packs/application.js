b9aa0f8cc40c531f7fbd41b04a0b4e0c36ff8237
dodellmitchell@gmail.com <YOUR@EMAIL.com>
2020-05-24 20:30:47 -0400

    Added fontawesome and bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import 'bootstrap';
+import '../stylesheets/application';
+
+document.addEventListener("turbolinks:load", () => {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+});
+import "@fortawesome/fontawesome-free/js/all"
